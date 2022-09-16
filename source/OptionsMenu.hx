28655419e3500eb689bf1f70f704bcb0c6b6873d
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-09-17 01:17:29 +0300

    bruh what?

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -138,13 +138,13 @@ class OptionsMenu extends MusicBeatState
 		addVirtualPad(LEFT_FULL, A_B_C);
 		#end
 
-		##if android
-		#var tipText:FlxText = new FlxText(10, 14, 0, 'Press C to customize your android controls', 16);
-		#tipText.setFormat(Paths.font('vcr.ttf'), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		#tipText.borderSize = 2.4;
-		#tipText.scrollFactor.set();
-		#add(tipText);
-		##end
+		#if android
+		var tipText:FlxText = new FlxText(10, 14, 0, 'Press C to customize your android controls', 16);
+		tipText.setFormat(Paths.font('vcr.ttf'), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		tipText.borderSize = 2.4;
+		tipText.scrollFactor.set();
+		add(tipText);
+		#end
 
 		super.create();
 	}
